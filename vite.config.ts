48310eaec80d45020659d5f38a25bc0f5420d2ab
Glider <2929799602@qq.com>
2023-12-03 23:22:00 +0800

    feat: 安装 unplugin 插件，ant-design, tailwindcss ui库

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import eslintPlugin from 'vite-plugin-eslint'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      imports: ['vue']
+    }),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false
+        })
+      ]
+    }),
     eslintPlugin({
       failOnError: false
     })
