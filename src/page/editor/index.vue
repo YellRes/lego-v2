a607b643b01ca8133c9dca2af0e8b86ec523f87b
Glider <2929799602@qq.com>
2023-12-04 23:01:49 +0800

    feat: 定义类型 editor页面的case组件UI

---
+++ src/page/editor/index.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import Case from './components/Case/index.vue'
+const commonStyle = {
+  background: 'transparent',
+  border: '1px solid #ededed'
+}
+
+const siderWidth = 400
+</script>
+
+<template>
+  <a-style-provider hash-priority="high">
+    <a-layout class="h-[100%]" :style="commonStyle">
+      <a-layout-header :style="commonStyle"></a-layout-header>
+
+      <a-layout>
+        <a-layout-sider
+          class="h-[100%] p-[8px]"
+          :style="commonStyle"
+          :width="siderWidth"
+        >
+          <Case />
+        </a-layout-sider>
+
+        <a-layout-content
+          class="h-[100%]"
+          :style="commonStyle"
+        ></a-layout-content>
+
+        <a-layout-sider
+          class="h-[100%]"
+          :style="commonStyle"
+          :width="siderWidth"
+        ></a-layout-sider>
+      </a-layout>
+    </a-layout>
+  </a-style-provider>
+</template>
+
+<style scoped></style>
