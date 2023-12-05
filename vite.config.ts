fe798d1d33dd06ca98aa895ffe395433c44c370e
Glider <2929799602@qq.com>
2023-12-05 17:58:23 +0800

    feat: 添加svg解法方案

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import path from 'path'
 import eslintPlugin from 'vite-plugin-eslint'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +23,10 @@ export default defineConfig({
     }),
     eslintPlugin({
       failOnError: false
+    }),
+    createSvgIconsPlugin({
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      symbolId: 'icon-[dir]-[name]'
     })
   ]
 })
