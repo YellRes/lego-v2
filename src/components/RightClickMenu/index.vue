1ad337aae4f4c295d735b8012ff76ab70ba9dfe4
Glider <2929799602@qq.com>
2023-12-14 17:35:32 +0800

    优化弹框容器的绑定事件逻辑

--- src/components/RightClickMenu/index.vue
+++ src/components/RightClickMenu/index.vue
@@ -21,37 +21,40 @@ const handleContextMenuClick = (e: IRightClickMenuOptionItem) => {
   isShow.value = false
   emits('clickMenuItem', e)
 }
-onMounted(() => {
-  // TODO: 为什么首次加载不触发
-  useEventListener(props.container, 'contextmenu', async (e: MouseEvent) => {
-    e.preventDefault()
-    isShow.value = true
-    await nextTick()
-    if (!rightClickMenuRef.value || !props.container) return
-    // 右击点到弹框后 不触发事件
 
-    const { clientX, clientY } = e
-    // 容器的左上角坐标
-    const { offsetTop, offsetLeft } = props.container
-    // 弹出菜单的宽高
-    const { clientHeight, clientWidth } = rightClickMenuRef.value
+watch(
+  () => props.container,
+  () => {
+    useEventListener(props.container, 'contextmenu', async (e: MouseEvent) => {
+      e.preventDefault()
+      isShow.value = true
+      await nextTick()
+      if (!rightClickMenuRef.value || !props.container) return
+      // 右击点到弹框后 不触发事件
 
-    // 鼠标在prop.container中的位置
-    const offsetX = clientX - offsetLeft
-    const offsetY = clientY - offsetTop
-    // 判断边界
-    rightClickMenuRef.value.style.top = `${
-      offsetY + clientHeight > props.container.clientHeight
-        ? offsetY - clientHeight
-        : offsetY
-    }px`
-    rightClickMenuRef.value.style.left = `${
-      offsetX + clientWidth > props.container.clientWidth
-        ? offsetX - clientWidth
-        : offsetX
-    }px`
-  })
-})
+      const { clientX, clientY } = e
+      // 容器的左上角坐标
+      const { offsetTop, offsetLeft } = props.container
+      // 弹出菜单的宽高
+      const { clientHeight, clientWidth } = rightClickMenuRef.value
+
+      // 鼠标在prop.container中的位置
+      const offsetX = clientX - offsetLeft
+      const offsetY = clientY - offsetTop
+      // 判断边界
+      rightClickMenuRef.value.style.top = `${
+        offsetY + clientHeight > props.container.clientHeight
+          ? offsetY - clientHeight
+          : offsetY
+      }px`
+      rightClickMenuRef.value.style.left = `${
+        offsetX + clientWidth > props.container.clientWidth
+          ? offsetX - clientWidth
+          : offsetX
+      }px`
+    })
+  }
+)
 
 onClickOutside(rightClickMenuRef, () => (isShow.value = false))
 </script>
