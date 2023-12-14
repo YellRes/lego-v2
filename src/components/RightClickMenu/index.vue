f86c0ef62d70926d71c543696979c553cf39b90a
Glider <2929799602@qq.com>
2023-12-14 08:02:08 +0800

    feat: 右键菜单组件构建

---
+++ src/components/RightClickMenu/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+interface IRightClickMenu {
+  container: HTMLElement | null
+  options: Array<{
+    label: string
+    eventName: string
+  }>
+}
+
+// const MENU_WIDTH = 100
+const props = withDefaults(defineProps<IRightClickMenu>(), {
+  container: null,
+  options: () => []
+})
+
+// const { x, y } = useMouseInElement(props.container)
+// useEventListener(props.container, '')
+</script>
+
+<template>
+  <div :class="`w-[200px] absolute`">
+    <a-list :data-source="props.options">
+      <template #renderItem="{ item }">
+        <a-card size="small">
+          {{ item.label }}
+        </a-card>
+      </template>
+    </a-list>
+  </div>
+</template>
